2c7b59771d76d953d8f4e91f134d6a2ad726fb2d
sojeon <sojeon@ifinfo.co.kr>
2023-10-31 12:58:19 +0900

    Writing A Custom List Component

--- src/users.tsx
+++ src/users.tsx
@@ -1,23 +1,31 @@
-import { List,SimpleList } from "react-admin";
+import { useMediaQuery, Theme} from "@mui/material";
+import { List,SimpleList, Datagrid, TextField, EmailField } from "react-admin";
 
-export const UserList = () => (
+export const UserList = () => {
+    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+    return (
     // <List>컴포넌트는 URL로부터 query파라미터를 읽고 이를 기반으로 API를 호출하며 React Context에 결과를 삽입
-    <List>
-        
-        {/* <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="username" />
-            <EmailField source="email" />
-            <TextField source="address.street" />
-            <TextField source="phone" />
-            <TextField source="website" />
-            <TextField source="company.name" />
-        </Datagrid> */}
-        <SimpleList
-            primaryText={(record) => record.name}
-            secondaryText={(record) => record.username}
-            tertiaryText={(record) => record.email}
-        />
-    </List>
-);
+        <List>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.name}
+                    secondaryText={(record) => record.username}
+                    tertiaryText={(record) => record.email}
+                />
+                ) : (
+                <Datagrid rowClick="edit">
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    <TextField source="username" />
+                    <EmailField source="email" />
+                    <TextField source="address.street" />
+                    <TextField source="phone" />
+                    <TextField source="website" />
+                    <TextField source="company.name" />
+                </Datagrid>
+                )}
+                
+            
+        </List>
+    );
+};
