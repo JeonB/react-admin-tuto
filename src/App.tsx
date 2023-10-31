b5020bfe3a46fc4d8e6b86092dce585d893095a3
sojeon <sojeon@ifinfo.co.kr>
2023-10-31 15:04:17 +0900

    Adding a Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
 import { PostEdit, PostList, PostCreate } from "./posts";
 import { Dashboard } from "./Dashboard";
-
+import { authProvider } from "./authProvider";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
@@ -11,7 +11,11 @@ import UserIcon from "@mui/icons-material/Group";
 // 데이터 교환을 위한 내가 정의한 API에 react-admin을 연결해주는 어P댑터
 // 사용자의 액션을 백엔드 API가 이해하는 HTTP request로 변환
 export const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    dashboard={Dashboard}
+  >
     <Resource
       name="posts"
       list={PostList}
