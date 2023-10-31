f3ab353d64bd6c2b47fc9a70c5a5d6129acd14dc
sojeon <sojeon@ifinfo.co.kr>
2023-10-31 10:51:03 +0900

    add anno

--- src/App.tsx
+++ src/App.tsx
@@ -8,4 +8,5 @@ import {
 import { dataProvider } from "./dataProvider";
 
 // dataProvider 속성만 정의
+// 데이터 교환을 위한 내가 정의한 API에 react-admin을 연결해주는 어댑터
 export const App = () => <Admin dataProvider={dataProvider}></Admin>;
