e0606774bc39380097d068b13fb39768ab5a9bf4
sojeon <sojeon@ifinfo.co.kr>
2023-10-31 13:58:50 +0900

    Handling Relationships

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import {
-  Admin,
-  Resource
-} from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
+import { PostList } from "./posts";
 
 // dataProvider 속성
-// 데이터 교환을 위한 내가 정의한 API에 react-admin을 연결해주는 어댑터
+// 데이터 교환을 위한 내가 정의한 API에 react-admin을 연결해주는 어P댑터
 // 사용자의 액션을 백엔드 API가 이해하는 HTTP request로 변환
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} />
+    <Resource name="users" list={UserList} recordRepresentation="name" />
   </Admin>
 );
