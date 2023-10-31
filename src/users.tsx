53adc990087494fed7055efdb1d48c6a6a428a80
sojeon <sojeon@ifinfo.co.kr>
2023-10-31 12:46:30 +0900

    Composing Components

--- src/users.tsx
+++ src/users.tsx
@@ -1,8 +1,10 @@
-import { List, Datagrid, TextField, EmailField } from "react-admin";
+import { List,SimpleList } from "react-admin";
 
 export const UserList = () => (
+    // <List>컴포넌트는 URL로부터 query파라미터를 읽고 이를 기반으로 API를 호출하며 React Context에 결과를 삽입
     <List>
-        <Datagrid rowClick="edit">
+        
+        {/* <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="name" />
             <TextField source="username" />
@@ -11,6 +13,11 @@ export const UserList = () => (
             <TextField source="phone" />
             <TextField source="website" />
             <TextField source="company.name" />
-        </Datagrid>
+        </Datagrid> */}
+        <SimpleList
+            primaryText={(record) => record.name}
+            secondaryText={(record) => record.username}
+            tertiaryText={(record) => record.email}
+        />
     </List>
 );
