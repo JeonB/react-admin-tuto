e1adaf0fde5e1e5b19eec7d53309759cd81530d3
sojeon <sojeon@ifinfo.co.kr>
2023-10-31 11:25:47 +0900

    Writing A Page Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import {
   Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
+  Resource
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
 
-// dataProvider 속성만 정의
+// dataProvider 속성
 // 데이터 교환을 위한 내가 정의한 API에 react-admin을 연결해주는 어댑터
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+// 사용자의 액션을 백엔드 API가 이해하는 HTTP request로 변환
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
