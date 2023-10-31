fa68a0cd39062df5d85cb0bd8e43dea754e1f576
sojeon <sojeon@ifinfo.co.kr>
2023-10-31 14:55:07 +0900

    Using a Custom Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Admin, Resource, ShowGuesser } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
 import { PostEdit, PostList, PostCreate } from "./posts";
+import { Dashboard } from "./Dashboard";
+
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
@@ -9,7 +11,7 @@ import UserIcon from "@mui/icons-material/Group";
 // 데이터 교환을 위한 내가 정의한 API에 react-admin을 연결해주는 어P댑터
 // 사용자의 액션을 백엔드 API가 이해하는 HTTP request로 변환
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource
       name="posts"
       list={PostList}
