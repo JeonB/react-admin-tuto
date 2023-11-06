01d0f35693dd737fbefbc5479d75704c26bd2ed1
JeonB <newjeonb@gmail.com>
2023-11-06 15:54:19 +0900

    Chip 컴포넌트 추가

---
+++ src/components/Chip.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Translate } from "@mui/icons-material";
+import {
+  Button,
+  Chip,
+  Collapse,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Popover,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+<Chip
+  label={getLabel()}
+  // color="primary"
+  // variant="outlined"
+  size="small"
+  onClick={handleClick}
+  onDelete={(event) => handleHide(event, source)}
+/>;
